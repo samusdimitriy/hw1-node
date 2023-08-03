55789b5e596c9fe58a508867dfbc4dd8b2428f16
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-08-03 10:50:58 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -16,31 +16,22 @@ async function listContacts() {
 }
 
 async function getContactById(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(data);
-    const contact = contacts.find((contact) => contact.id === contactId);
-    return contact;
-  } catch {
-    return null;
-  }
+  const data = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(data);
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
 }
 
 async function removeContact(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(data);
-    const index = contacts.findIndex((contact) => contact.id === contactId);
-    if (index === -1) {
-      return null;
-    }
-    const removedContact = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return removedContact;
-  } catch (error) {
-    console.error("Error removing contact:", error.message);
+  const data = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(data);
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
     return null;
   }
+  const removedContact = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removedContact[0] || null;
 }
 
 async function addContact(name, email, phone) {
